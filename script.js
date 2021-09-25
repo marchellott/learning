cfbce9a8a7654a5583640c37e1aa2f8fb3fa70f2
Mark <marktm00.mt@gmail.com>
2021-09-25 11:29:13 +0300

    первая часть урока - создание приложения - переменные

--- script.js
+++ script.js
@@ -2,4 +2,21 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 /* Объект и в нем свойства */
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
+/* Задать два вопроса и поместить ответы в переменные */
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
